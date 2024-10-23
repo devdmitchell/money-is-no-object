f42d6563d257020ec7a8043912873d96acd16a51
devdmitchell <dmitchell.prsvr@gmail.com>
2024-10-22 20:21:58 -0500

    completed

--- main.js
+++ main.js
@@ -2,6 +2,38 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+const isAdmin = function(user){
+    return user.userRole === 'ADMIN'
+}
+
+const getEmail = function(user){
+    return `${user.firstName[0]}${user.lastName}.prsvr@gmail.com`.toLowerCase()
+}
+
+const getPlaylistLength = function(playlist){
+    return playlist.songs.length
+    // return playlist['songs'].length    
+}
+
+const getHardestHomework = function(homework){
+    let hardest = ''
+    let lowestScore = 9000
+    for (let assignment of homework){
+        if (assignment.averageScore < lowestScore) {
+            lowestScore = assignment.averageScore
+            hardest = assignment.name
+        }
+    }
+    return hardest
+}
+
+const createPhonebook = function(dir1, dir2) {
+    let phonebook = {}
+    for (let i = 0; i < dir1.length; i++) {
+        phonebook[dir1[i]] = dir2[i]
+    }
+    return phonebook
+}
 
 
 
